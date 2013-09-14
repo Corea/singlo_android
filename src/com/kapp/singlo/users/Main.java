dba1da89242b028e09de66144a8b5e3288b847f0
Hyungsuk Yoon <youhanmir@gmail.com>
2013-09-14 18:37:22 +0900

    K-App 변경사항

--- src/com/kapp/singlo/users/Main.java
+++ src/com/kapp/singlo/users/Main.java
@@ -1,16 +1,22 @@
 package com.kapp.singlo.users;
 
-import com.google.android.gcm.GCMRegistrar;
-import com.kapp.singlo.GCMIntentService;
-import com.kapp.singlo.R;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
+import com.google.android.gcm.GCMRegistrar;
+import com.kapp.singlo.GCMIntentService;
+import com.kapp.singlo.R;
+import com.kapp.singlo.http.HttpPost;
 
 public class Main extends Activity {
 	private Button registerButton;
@@ -22,29 +28,55 @@ public class Main extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		// GCM 등록
-		String regId = GCMRegistrar.getRegistrationId(getApplicationContext());
-		if ("".equals(regId)) {
-			GCMRegistrar.register(getApplicationContext(),
-					GCMIntentService.getProjectId());
-		}
-		
-		SharedPreferences spLogin;
-		spLogin = getSharedPreferences("login", Login.MODE_PRIVATE);
-		int id = spLogin.getInt("id", 0);
-
-		if (id != 0) {
-			Intent intent = new Intent(Main.this, Login.class);
-			startActivity(intent);
-			finish();
-		}
-
 		registerButton = (Button) findViewById(R.id.RegisterButton);
 		registerButton.setOnClickListener(registerButtonClickListener);
 		loginButton = (Button) findViewById(R.id.LoginButton);
 		loginButton.setOnClickListener(loginButtonClickListener);
 
 		mainActivity = Main.this;
+		
+		// main thread 에서 http post 사용하기 위해 선언.
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+				
+		if (HttpPost.check_version(mainActivity.getBaseContext())==true) { // 버젼체크
+			// GCM 등록
+			String regId = GCMRegistrar.getRegistrationId(getApplicationContext());
+			if ("".equals(regId)) {
+				GCMRegistrar.register(getApplicationContext(),
+						GCMIntentService.getProjectId());
+			}
+			
+			SharedPreferences spLogin;
+			spLogin = getSharedPreferences("login", Login.MODE_PRIVATE);
+			int id = spLogin.getInt("id", 0);
+
+			if (id != 0) {
+				Intent intent = new Intent(Main.this, Login.class);
+				startActivity(intent);
+				finish();
+			}
+		} else {
+			new AlertDialog.Builder(this)
+			.setIcon(R.drawable.icon)
+			.setTitle("버젼 정보 안내")
+			.setMessage("최신버젼이 등록되었습니다. 업데이트후 접속하실수 있습니다.")
+			.setPositiveButton("예", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.kapp.timelotto")));
+					android.os.Process.killProcess(android.os.Process.myPid());
+				}
+			})
+			.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					android.os.Process.killProcess(android.os.Process.myPid());					
+				}
+			})
+			.show();
+		}
+				
 	}
 
 	OnClickListener registerButtonClickListener = new OnClickListener() {
